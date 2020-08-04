0366aa4682814b90dc5a2fab25b674c9010aa0a6
zhaoyl <htzhao200744@gmail.com>
2020-08-05 07:49:17 +0800

    [javascript] add build_in_function.js

---
+++ javascript/build_in_functions/build_in_function.js
@@ -0,0 +1,14 @@
+/**
+ * Created by betty on 8/5/20.
+ */
+"use strict";
+
+// 执行一段string, string为js代码
+eval("console.log('aaa')")
+// par
+console.log(parseInt("100", 10))
+//
+console.log(parseFloat("100.1"))
+// 
+let url = decodeURI("http://www.google.com")
+console.log(url)
