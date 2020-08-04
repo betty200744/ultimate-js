53128d342229af4baa0be88b9ad4793bbbfd6815
zhaoyl <htzhao200744@gmail.com>
2020-08-04 14:18:46 +0800

    [javascript] add declarations.js

---
+++ javascript/declarations/declarations.js
@@ -0,0 +1,12 @@
+/**
+ * Created by betty on 8/4/20.
+ */
+"use strict";
+
+const a = 1 // const
+var b = 2; // local or global variables
+let c = 3; // block-scope variables
+console.log(a, b, c)
+
+
+
---
+++ javascript/declarations/variable_hoisting.js
@@ -0,0 +1,8 @@
+/**
+ * Created by betty on 8/4/20.
+ */
+"use strict";
+
+// var 才能遍历提升
+console.log(x === undefined)
+var x = 2;
