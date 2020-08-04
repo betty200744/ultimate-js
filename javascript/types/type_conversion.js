10ad69fe7ac1dff739e5d910ce46c6e1610f72b5
zhaoyl <htzhao200744@gmail.com>
2020-08-04 14:18:34 +0800

    [javascript] add types.js

---
+++ javascript/types/type_conversion.js
@@ -0,0 +1,21 @@
+/**
+ * Created by betty on 8/4/20.
+ */
+"use strict";
+
+let a = 42
+a = 'abc'
+console.log( typeof a, ", value: ")
+
+// number > string
+let b = "this is " + 2
+console.log(typeof b)
+
+// string > number
+let c = 42
+console.log(42 - '37')
+
+// converting string to numbers
+console.log(parseInt('101', 10))
+
+
---
+++ javascript/types/types.js
@@ -0,0 +1,25 @@
+/**
+ * Created by betty on 8/4/20.
+ */
+"use strict";
+
+/*
+
+Boolean. true and false.
+null. A special keyword denoting a null value
+undefined. A top-level property whose value is not defined.
+Number. An integer or floating point number. For example: 42 or 3.14159.
+BigInt. An integer with arbitrary precision. For example: 9007199254740992n.
+String. A sequence of characters that represent a text value. For example: "Howdy"
+Symbol (new in ECMAScript 2015). A data type whose instances are unique and immutable.
+
+* */
+
+let a = true
+let b = null
+let c = undefined
+let d = 33
+let e = 9007199254740992
+let f = "hello"
+
+console.log(a, b, c, d, e, f)
