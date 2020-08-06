1ba3266f495bc8c2fc77ddafd96c4cc8c7e017be
zhaoyl <htzhao200744@gmail.com>
2020-08-06 20:57:12 +0800

    [design-patterns] update

--- design-patterns/creational/abstract_factory/abstract_factory.js
+++ design-patterns/creational/abstract_factory/abstract_factory.js
@@ -1,4 +1,4 @@
-// 发一组相同类型的object放在一个类里面， 之后用class method来获取具体的类
+// 相同类型的object一组里面即放在一个类里面， 之后用class method来获取具体的类
 // groups object factories that have a common theme
 class CarFactory {
   constructor() {
