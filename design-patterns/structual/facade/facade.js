00a918e378e4c21b2047242a984199021f2edebf
zhaoyl <htzhao200744@gmail.com>
2020-08-07 10:09:35 +0800

    [design-patterns] update

---
+++ design-patterns/structual/facade/facade.js
@@ -0,0 +1,6 @@
+/**
+ * Created by betty on 8/7/20.
+ */
+"use strict";
+
+// provides a simplified interface to a large body of code.
---
+++ design-patterns/structual/flyweight/flyweight.js
@@ -0,0 +1,5 @@
+/**
+ * Created by betty on 8/7/20.
+ */
+"use strict";
+// reduces the cost of creating and manipulating a large number of similar objects.
