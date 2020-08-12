fd445c966ece191aba47ce2bfef573b81485e234
zhaoyl <htzhao200744@gmail.com>
2020-08-12 13:35:29 +0800

    [algorithms] add avlTree.ts

--- algorithm/data-structures/tree/avlTree.ts
+++ algorithm/data-structures/tree/avlTree.ts
@@ -1,6 +1,21 @@
 import {BinarySearchTree} from "./binarySearchTree";
 import {BinarySearchTreeNode} from "./binarySearchTreeNode";
 
+
+/*
+*
+*
+* Balanced Search Trees:
+There are many balanced search trees.
+AVL Trees Adel’son-Velsii and Landis 1962
+B-Trees/2-3-4 Trees Bayer and McCreight 1972 (see CLRS 18)
+BB[α] Trees Nievergelt and Reingold 1973
+Red-black Trees CLRS Chapter 13
+(A) — Splay-Trees Sleator and Tarjan 1985
+(R) — Skip Lists Pugh 1989
+(A) — Scapegoat Trees Galperin and Rivest 1993
+(R) — Treaps Seidel and Aragon 1996
+* */
 // require heights of the left & right of children of every node to diff by at most + or - 1
 class AvlTree extends BinarySearchTree {
     constructor() {
