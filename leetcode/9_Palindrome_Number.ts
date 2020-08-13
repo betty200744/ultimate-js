77957b4e7a845cee022f7243afe69cd17888c9f9
zhaoyl <htzhao200744@gmail.com>
2020-08-13 11:07:14 +0800

    [leetcode] add 9_Palindrome_Number.ts

--- leetcode/9_Palindrome_Number.ts
+++ leetcode/9_Palindrome_Number.ts
@@ -1,23 +1,12 @@
-import {reverse} from "./7_reverse";
-
-function isEven(x: number): boolean {
-    return x % 2 === 0
-}
 
 export function isPalindrome(x: number): boolean {
-    if (x < 0) {
-        return false
+    if (x < 0 || (x % 10 === 0 && x !== 0)) {
+        return false;
     }
-    let s = String(x)
-    let haft = s.length / 2
-    let left: string
-    let right: string
-    if (x % 2 === 0) {
-        left = s.slice(0, haft)
-        right = s.slice(haft)
-    } else {
-        left = s.slice(0, haft)
-        right = s.slice(haft + 1, s.length)
+    let num = 0;
+    while (x > num) {
+        num = num * 10 + (x % 10);
+        x = (x - (x % 10)) / 10;
     }
-    return Number(left) === reverse(Number(right))
+    return x === num || x === (num - (num % 10)) / 10;
 }
