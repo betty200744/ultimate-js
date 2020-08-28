53fa2894d7ec2a7e201de62c06f63cd6eac5430f
zhaoyl <htzhao200744@gmail.com>
2020-08-28 19:25:33 +0800

    [docs] up

--- algorithm/data-structures/stack/stack.test.ts
+++ algorithm/data-structures/stack/stack.test.ts
@@ -1,12 +1,5 @@
 describe('Stack', async () => {
     it('Stack', async () => {
-        // Last-in, First-out
-        let s: number[] = []
-        // Push
-        s.push(1)
-        s.push(2)
-        // Pop
-        let res = s.pop()
-        expect(res).toEqual(2)
+
     });
 })
