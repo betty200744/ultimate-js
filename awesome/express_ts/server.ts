39dc838e2e91b17aad73378731a159d4fcacf17f
zhaoyl <htzhao200744@gmail.com>
2020-08-28 08:38:41 +0800

    [docs] add middleware order test

--- awesome/express_ts/server.ts
+++ awesome/express_ts/server.ts
@@ -2,6 +2,23 @@ import * as express from 'express'
 
 const port = 3000
 const app: express.Application = express();
+
+let myLogger1 = function (req, res, next) {
+    console.log("myLogger1 , before")
+    next()
+    console.log("myLogger1, after")
+}
+let myLogger2 = function (req, res, next) {
+    console.log("myLogger2 , before")
+    next()
+    console.log("myLogger2, after")
+}
+let myLogger3 = function (req, res, next) {
+    console.log("myLogger3 , before")
+    next()
+    console.log("myLogger3, after")
+}
+app.use(myLogger1, myLogger2, myLogger3)
 app.get("/", function (req, res) {
     res.send('hello, world')
 })
